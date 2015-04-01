ee1752cebc081d35e4e8773ae2d9bf70d9f69c4f
Augusto Salazar <salazar.augusto@gmail.com>
2015-04-01 10:12:01 -0500

    Organize imports

--- app/src/main/java/com/augustosalazar/androidsensors/MainActivity.java
+++ app/src/main/java/com/augustosalazar/androidsensors/MainActivity.java
@@ -1,22 +1,11 @@
 package com.augustosalazar.androidsensors;
 
-import java.util.Locale;
-
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.os.Bundle;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -49,6 +38,8 @@ public class MainActivity extends ActionBarActivity {
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.pager);
         mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        mViewPager.setPageTransformer(true, new DepthPageTransformer());
 
     }
 
