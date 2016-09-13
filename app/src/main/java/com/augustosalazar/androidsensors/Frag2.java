07830b1e20608fc362fd5e0f74b3e6864bc525aa
Augusto Salazar <augustosalazar@uninorte.edu.co>
2016-09-13 10:52:33 -0500

    Nuevo permisos

--- app/src/main/java/com/augustosalazar/androidsensors/Frag2.java
+++ app/src/main/java/com/augustosalazar/androidsensors/Frag2.java
@@ -1,14 +1,18 @@
 package com.augustosalazar.androidsensors;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,8 +20,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.location.LocationRequest;
+
+import java.util.List;
 
 
 public class Frag2 extends Fragment implements LocationListener {
@@ -28,10 +35,11 @@ public class Frag2 extends Fragment implements LocationListener {
     private Button mButton;
     private boolean mStarted;
     private LocationManager mLocationManager;
+    private final int MY_PERMISSIONS_REQUEST = 1;
 
     private static final String ARG_SECTION_NUMBER = "section_number";
 
-
+    private String TAG = "Flag2";
 
     public static Frag2 newInstance(int sectionNumber) {
         Frag2 fragment = new Frag2();
@@ -61,7 +69,7 @@ public class Frag2 extends Fragment implements LocationListener {
             public void onClick(View v) {
                 if (!mStarted) {
                     startCapture(getActivity());
-                }else {
+                } else {
                     stopCapturing();
                 }
             }
@@ -70,8 +78,26 @@ public class Frag2 extends Fragment implements LocationListener {
         return rootView;
     }
 
+    public String getBestProvider(LocationManager lm) {
+        List<String> provs = lm.getAllProviders();
+        for (String prov : provs) {
+            Log.d(TAG, "Provider: " + prov);
+        }
+        Criteria c = new Criteria();
+        c.setAccuracy(Criteria.ACCURACY_COARSE);
+        c.setAltitudeRequired(false);
+        c.setBearingRequired(false);
+        c.setCostAllowed(false);
+        c.setPowerRequirement(Criteria.NO_REQUIREMENT);
+        c.setSpeedRequired(false);
+        String provider = lm.getBestProvider(c, true);
+        Log.d(TAG, "Best Provider: " + provider);
+        return provider;
+    }
 
-    public void startCapture(Context context){
+
+
+    public void startCapture(Context context) {
         mStarted = true;
         mButton.setText("Detener Sensor");
 
@@ -110,17 +136,27 @@ public class Frag2 extends Fragment implements LocationListener {
         }
 
 
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "SIN PERMISOS");
 
+            mStarted = false;
+            mButton.setText("Iniciar Sensor");
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                    MY_PERMISSIONS_REQUEST);
+            return;
+        }
+        mLocationManager.requestLocationUpdates(getBestProvider(mLocationManager),0, 0, this);
 
-        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                0, 0, this);
 
     }
 
     public void stopCapturing() {
         mStarted = false;
         mButton.setText("Iniciar Sensor");
-
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
         if (mLocationManager != null) {
             mLocationManager.removeUpdates(this);
         }
@@ -147,5 +183,28 @@ public class Frag2 extends Fragment implements LocationListener {
     @Override
     public void onProviderDisabled(String provider) {
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        Log.d(TAG, "MainActivity onRequestPermissionsResult requestCode" +requestCode);
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Toast.makeText(getActivity(),"Perfecto, volver a internar",Toast.LENGTH_LONG).show();
+
+
+                } else {
+
+                    Toast.makeText(getActivity(),"???",Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+
+        }
     }
 }
